05cf48773ac68353b95f7e58464da9eb0dc3de8e
Anjali Gopal <70918578+anjalikarthikeyan@users.noreply.github.com>
2023-12-22 11:57:14 +0530

    Revert "Merge branch 'main' into UpdateEntry"
    This reverts commit 5d505e1b6e8a91c5327c2cb3b75b57e8283f2e09, reversing
    changes made to 7243d6b3a7fbd446d1a9a09c05eeddc01aef017d.

--- src/main/java/com/shorewise/wiseconnect/router/routing/DataStoreToActiveMqRoute.java
+++ src/main/java/com/shorewise/wiseconnect/router/routing/DataStoreToActiveMqRoute.java
@@ -1,49 +1,70 @@
 package com.shorewise.wiseconnect.router.routing;
 
+import java.util.List;
+import org.apache.camel.Exchange;
 import org.apache.camel.ExchangePattern;
+import org.apache.camel.LoggingLevel;
 import org.apache.camel.builder.RouteBuilder;
+import org.apache.camel.model.rest.RestBindingMode;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.apache.camel.model.rest.RestBindingMode;
+import com.shorewise.wiseconnect.router.repository.TransactionalXmlRepository;
+import com.shorewise.wiseconnect.router.entity.TransactionalXml;
 
 @Component
 public class DataStoreToActiveMqRoute extends RouteBuilder {
 
+    @Autowired
+    private TransactionalXmlRepository transactionalXmlRepository;
+
     @Override
     public void configure() {
-    	
-    	// Configure REST DSL to use swagger
+        // Configure REST DSL for Swagger support
         restConfiguration()
             .component("servlet")
             .bindingMode(RestBindingMode.json)
             .dataFormatProperty("prettyPrint", "true")
             .apiContextPath("/api-doc")
-            .apiProperty("api.title", "DataStore to ActiveMQ API")
-            .apiProperty("api.version", "1.0");
+                .apiProperty("api.title", "DataStore to ActiveMQ Route")
+                .apiProperty("api.version", "1.0");
 
-        // REST endpoint to trigger the process
-        rest("/wiseconnect")
-            .post("/transactions/extract-and-sendTomq")
-            .consumes(MediaType.APPLICATION_XML_VALUE)
+        // Define REST endpoint
+        rest("/datastore")
+            .post("/toActiveMQ")
             .produces(MediaType.APPLICATION_JSON_VALUE)
-            .to("direct:readAndSendToActiveMQ");
+            .route()
+                .to("direct:readAndSendToActiveMQ")
+            .endRest();
 
-     // Route to read, process, and send records to ActiveMQ
+        // Define Camel route
         from("direct:readAndSendToActiveMQ")
             .routeId("datastore-to-activemq-route")
-            .log("Reading 'Received' records from the database")
-            // SQL query to fetch 'received' records
-            .to("sql:select xml_data from xml_storage.transactional_xml where status = 'Received'")
-            .split(body()) // Split for processing multiple rows
-                .log("Sending XML record to ActiveMQ")
+            .log(LoggingLevel.INFO, "Fetching 'received' records from the database")
+            .bean(this, "fetchRecords")
+            .split(body())  // Serialize to XML
+                .log(LoggingLevel.INFO, "Sending record to ActiveMQ")
+                // Removed JSON marshaling, handle as XML
                 .setExchangePattern(ExchangePattern.InOnly)
-                .to("activemq:queue:Processed") // Send to ActiveMQ queue
-                .log("Record sent to ActiveMQ")
-                // Update record status in the database
-                .process(exchange -> log.info("Received XML message:", exchange.getIn().getBody(String.class)))           
-                .to("sql:update xml_storage.transactional_xml set status = 'Processed'")
-                .process(exchange -> log.info("Message forwarded to ActiveMQ queue: Ingress"))
+                .to("activemq:queue:ProcessedQueueData")
+                .bean(this, "updateRecordStatus")
+                .log(LoggingLevel.INFO, "Record status updated to 'processed'")
             .end();
     }
-               
+
+    public List<TransactionalXml> fetchRecords() {
+        List<TransactionalXml> records = transactionalXmlRepository.findByStatus("Received");
+        if (records.isEmpty()) {
+            log.info("No 'received' records found in the database.");
+        } else {
+            records.forEach(record -> log.info("Fetched record with ID: {}", record.getId()));
+        }
+        return records;
+    }
+
+    public void updateRecordStatus(TransactionalXml record) {
+        record.setStatus("Processed");
+        transactionalXmlRepository.save(record);
+        log.info("Updated record status to 'processed' for ID: {}", record.getId());
+    }
 }
